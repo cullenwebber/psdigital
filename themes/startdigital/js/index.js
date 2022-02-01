ee57938ffef035f5826fb7115343e9a917e1ae0a
Joe <joe@startdigital.com.au>
2022-02-01 11:09:11 +0800

    add some more defaults

--- themes/startdigital/js/index.js
+++ themes/startdigital/js/index.js
@@ -1,3 +1,42 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("hey jake");
-});
+document.addEventListener('DOMContentLoaded', () => {
+	toggleMenu()
+	toggleMobileSubMenu()
+})
+
+/**
+ * Toggle the state of the mobile menu
+ *
+ * @returns void
+ */
+const toggleMenu = () => {
+	const menuButtons = document.querySelectorAll('[data-toggle-menu]')
+
+	menuButtons.forEach((btn) => {
+		btn.addEventListener('click', () =>
+			document.body.classList.toggle('menuIsOpen')
+		)
+	})
+}
+
+/**
+ * Toggles the mobile sub menu
+ *
+ * @returns void
+ */
+const toggleMobileSubMenu = () => {
+	const toggles = document.querySelectorAll('[data-toggle-mobile-sub-menu]')
+
+	toggles.forEach((toggle) => {
+		toggle.addEventListener('click', () => {
+			const parentEl = toggle.closest('[data-mobile-menu-item]')
+			const subMenu = parentEl.querySelector('[data-mobile-sub-menu]')
+
+			// Doesn't exist, bail early
+			if (!subMenu) {
+				return
+			}
+
+			subMenu.classList.toggle('hidden')
+		})
+	})
+}
