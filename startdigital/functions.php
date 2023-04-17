0d5f33fa8bb3d9c625d8b8c4690ef01395a391c0
Tom Brennan <67259512+tomcbrennan@users.noreply.github.com>
2023-04-17 13:38:33 +0800

    Remove author from post social embeds

--- startdigital/functions.php
+++ startdigital/functions.php
@@ -160,4 +160,13 @@ class StartDigital extends Timber\Site
 	}
 }
 
+// REMOVE AUTHOR FROM POST SOCIAL EMBEDS
+
+add_filter( 'oembed_response_data', 'disable_embeds_filter_oembed_response_data_' );
+function disable_embeds_filter_oembed_response_data_( $data ) {
+    unset($data['author_url']);
+    unset($data['author_name']);
+    return $data;
+}
+
 new StartDigital();
