758fcf6a9ea68bc995581bb89fb3f0a2316c5860
Tom Brennan <67259512+tomcbrennan@users.noreply.github.com>
2023-05-19 09:16:27 +0800

    Tease clean up + remove archive

--- startdigital/index.php
+++ startdigital/index.php
@@ -15,9 +15,10 @@
 
 $context          = Timber::context();
 $context['posts'] = new Timber\PostQuery();
-$context['foo']   = 'bar';
+$context['fields'] = get_fields(get_option('page_for_posts'));
 $templates        = array( 'index.twig' );
 if ( is_home() ) {
 	array_unshift( $templates, 'front-page.twig', 'home.twig' );
 }
+
 Timber::render( $templates, $context );
