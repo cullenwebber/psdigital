7ec0dd7a9608dd6a81c628b74b24b295bdc9b1b7
Joe Grant ⚡ <jmgrant1995@gmail.com>
2023-08-14 11:41:58 +0800

    Update image-focal-point.php

--- startdigital/helpers/image-focal-point/image-focal-point.php
+++ startdigital/helpers/image-focal-point/image-focal-point.php
@@ -1,7 +1,8 @@
 <?php
 
 // Custom media field
-function custom_media_add_media_custom_field($form_fields, $post) {
+function custom_media_add_media_custom_field($form_fields, $post)
+{
     $screen_sizes = array('desktop', 'mobile');
 
     $html = "";
@@ -13,7 +14,7 @@ function custom_media_add_media_custom_field($form_fields, $post) {
         $field_value = ($field_value) ? $field_value : '50% 50%';
 
         $html .= "
-            <input type='hidden' value='{$field_value}' id='bg_pos_{$size}_id' name='attachments[{$post->ID}][bg_pos_{$size}]'>
+            <input type='hidden' value='{$field_value}' id='bg_pos_{$size}_id' name='attachments[{$post->ID}][bg_pos_{$size}]' data-attachment-id='$post->ID'>
             <div class='focusp_label_holder'>
                 <div id='{$size}_value'>{$size_text}</div>
                 <input type='button' class='button button-small' value='{$label}' id='label_{$size}' onclick='setFocus(\"{$size}\")'>
@@ -30,7 +31,7 @@ function custom_media_add_media_custom_field($form_fields, $post) {
                         <h3>Click on the image to set the focus point</h3>
                         <div class='controls'>
                             <span class='button button-secondary' onclick='cancelFocus()'>Cancel</span>
-                            <span class='button button-primary' onclick='closeOverlay()'>Save</span>
+                            <span class='button button-primary' onclick='closeOverlay(event)'>Save</span>
                         </div>
                     </div>
                 </div>
@@ -54,22 +55,26 @@ function custom_media_add_media_custom_field($form_fields, $post) {
 }
 add_filter('attachment_fields_to_edit', 'custom_media_add_media_custom_field', null, 2);
 
+function update_attachment_bg_position()
+{
+    $attachment_id = $_POST['attachment_id'];
+    $bg_pos_desktop = $_POST['bg_pos_desktop'];
+    $bg_pos_mobile = $_POST['bg_pos_mobile'];
 
-// Save custom media field
-function custom_media_save_attachment($attachment_id) {
-    $screen_sizes = array('desktop', 'mobile');
+    update_post_meta($attachment_id, "bg_pos_desktop", $bg_pos_desktop);
+    update_post_meta($attachment_id, "bg_pos_mobile", $bg_pos_mobile);
 
-    foreach ($screen_sizes as $size) {
-        if (isset($_REQUEST['attachments'][$attachment_id]["bg_pos_{$size}"])) {
-            $bg_pos = $_REQUEST['attachments'][$attachment_id]["bg_pos_{$size}"];
-            update_post_meta($attachment_id, "bg_pos_{$size}", $bg_pos);
-        }
-    }
+    echo 'success';
+    wp_die(); // this is required to terminate immediately and return a proper response
 }
-add_action('edit_attachment', 'custom_media_save_attachment');
+
+add_action('wp_ajax_update_attachment_bg_position', 'update_attachment_bg_position');
+add_action('wp_ajax_nopriv_update_attachment_bg_position', 'update_attachment_bg_position');
+
 
 // Apply filter in frontend to object-position
-function filter_gallery_img_atts($atts, $attachment) {
+function filter_gallery_img_atts($atts, $attachment)
+{
     $screen_sizes = array('desktop', 'mobile');
     $bg_pos_styles = array();
 
@@ -91,14 +96,15 @@ add_filter('wp_get_attachment_image_attributes', 'filter_gallery_img_atts', 10, 
 
 
 //enqueue script in admin
-function image_bg_admin_scripts() {
+function image_bg_admin_scripts()
+{
     $file_path = __DIR__;
     $url_path = str_replace($_SERVER['DOCUMENT_ROOT'], '', $file_path);
-    wp_enqueue_style( 'image-bg-css', $url_path . '/image-focal-point.css' );
-    wp_enqueue_script( 'image-bg-js', $url_path . '/image-focal-point.js' );
+    wp_enqueue_style('image-bg-css', $url_path . '/image-focal-point.css');
+    wp_enqueue_script('image-bg-js', $url_path . '/image-focal-point.js', array(), '');
 }
 
-add_action( 'admin_enqueue_scripts', 'image_bg_admin_scripts' );
+add_action('admin_enqueue_scripts', 'image_bg_admin_scripts');
 
 /**
  * Get an attachment ID given a URL.
@@ -109,49 +115,47 @@ add_action( 'admin_enqueue_scripts', 'image_bg_admin_scripts' );
  *
  * @return int Attachment ID on success, 0 on failure
  */
-function get_attachment_id($url) {
+function get_attachment_id($url)
+{
 
-	$attachment_id = 0;
+    $attachment_id = 0;
 
-	$dir = wp_upload_dir();
+    $dir = wp_upload_dir();
 
-	if (false !== strpos( $url, $dir['baseurl'] . '/')) { // Is URL in uploads directory?
-		$file = basename($url);
+    if (false !== strpos($url, $dir['baseurl'] . '/')) { // Is URL in uploads directory?
+        $file = basename($url);
 
-		$query_args = array(
-			'post_type'   => 'attachment',
-			'post_status' => 'inherit',
-			'fields'      => 'ids',
-			'meta_query'  => array(
-				array(
-					'value'   => $file,
-					'compare' => 'LIKE',
-					'key'     => '_wp_attachment_metadata',
-				),
-			)
-		);
+        $query_args = array(
+            'post_type'   => 'attachment',
+            'post_status' => 'inherit',
+            'fields'      => 'ids',
+            'meta_query'  => array(
+                array(
+                    'value'   => $file,
+                    'compare' => 'LIKE',
+                    'key'     => '_wp_attachment_metadata',
+                ),
+            )
+        );
 
-		$query = new WP_Query($query_args);
+        $query = new WP_Query($query_args);
 
-		if ($query->have_posts()) {
+        if ($query->have_posts()) {
 
-			foreach ($query->posts as $post_id) {
+            foreach ($query->posts as $post_id) {
 
-				$meta = wp_get_attachment_metadata($post_id);
+                $meta = wp_get_attachment_metadata($post_id);
 
-				$original_file = basename($meta['file']);
-				$cropped_image_files = wp_list_pluck($meta['sizes'], 'file');
+                $original_file = basename($meta['file']);
+                $cropped_image_files = wp_list_pluck($meta['sizes'], 'file');
 
-				if ($original_file === $file || in_array( $file, $cropped_image_files)) {
-					$attachment_id = $post_id;
-					break;
-				}
+                if ($original_file === $file || in_array($file, $cropped_image_files)) {
+                    $attachment_id = $post_id;
+                    break;
+                }
+            }
+        }
+    }
 
-			}
-
-		}
-
-	}
-
-	return $attachment_id;
+    return $attachment_id;
 }
