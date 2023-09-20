3047727e238146fc8eb144ebc281da4f6fce8b17
Joe Grant <jmgrant1995@gmail.com>
2023-09-20 09:38:47 +0800

    woop

--- wp-content/themes/startdigital/functions.php
+++ wp-content/themes/startdigital/functions.php
@@ -179,4 +179,20 @@ function custom_login_logo()
 }
 add_action('login_enqueue_scripts', 'custom_login_logo');
 
+/**
+ * Enable features from Soil when plugin is activated
+ * @link https://roots.io/plugins/soil/
+ */
+add_theme_support('soil', [
+	'clean-up',
+	'disable-rest-api',
+	'disable-asset-versioning',
+	'disable-trackbacks',
+	'google-analytics' => 'UA-XXXXX-Y',
+	'js-to-footer',
+	'nav-walker',
+	'nice-search',
+	'relative-urls'
+]);
+
 new StartDigital();
