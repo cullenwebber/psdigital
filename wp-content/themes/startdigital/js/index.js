10932175fb7a264fae57d87dfada26f683fd1805
Tom Brennan <67259512+tomcbrennan@users.noreply.github.com>
2024-09-20 11:32:46 +0800

    swap all sliders to embla slider with defaults

--- wp-content/themes/startdigital/js/index.js
+++ wp-content/themes/startdigital/js/index.js
@@ -1,15 +1,13 @@
 import animateOnScroll from './utils/animate-on-scroll'
 import initAccordions from './components/accordions'
-import initSliders from './components/sliders'
+import initSliders from './components/sliders/sliders'
 import initRemoveLinks from './utils/removeLink'
 import initMenus from './components/menu'
-import initInfiniteScroll from './utils/infiniteScroll'
 
 document.addEventListener('DOMContentLoaded', () => {
 	animateOnScroll()
 	initMenus()
 	initSliders()
-	initInfiniteScroll()
 	initAccordions()
 	initRemoveLinks()
 })
