b11095f7327296c7b35a3152fb93281df0b62907
Tom Brennan <67259512+tomcbrennan@users.noreply.github.com>
2024-10-11 14:44:08 +0800

    submenu + mobile menu + animations refine

--- wp-content/themes/startdigital/js/utils/animate-on-scroll.js
+++ wp-content/themes/startdigital/js/utils/animate-on-scroll.js
@@ -4,184 +4,63 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger'
 gsap.registerPlugin(ScrollTrigger)
 
 export default function animateOnScroll() {
-	/**
-	 * SINGLE ELEMENT SELECTORS
-	 * Apply these data attributes DIRECTLY to desired elements :~)
-	 */
-	const elementsFadeDown = document.querySelectorAll('[data-fade-down]')
-	const elementsFadeUp = document.querySelectorAll('[data-fade-up]')
-	const elementsFadeLeft = document.querySelectorAll('[data-fade-left]')
-	const elementsFadeRight = document.querySelectorAll('[data-fade-right]')
+	// ELEMENT SELECTORS ANIMATE
 	const elementsFade = document.querySelectorAll('[data-fade]')
 
-	/**
-	 * MULTIPLE ELEMENT SELECTORS
-	 * Apply these data attributes to CONTAINER of desired elements :~)
-	 */
-	const elementsStaggerFade = document.querySelectorAll(
-		'[data-animate-stagger-fade]'
-	)
-	const elementsStaggerRight = document.querySelectorAll(
-		'[data-animate-stagger-right]'
-	)
-	const elementsStaggerLeft = document.querySelectorAll(
-		'[data-animate-stagger-left]'
-	)
-	const elementsStaggerUp = document.querySelectorAll(
-		'[data-animate-stagger-up]'
-	)
-	const elementsStaggerDown = document.querySelectorAll(
-		'[data-animate-stagger-down]'
-	)
+	// ELEMENT SELECTOR ANIMATE CHILDREN
+	const elementsStagger = document.querySelectorAll('[data-stagger]')
 
-	const START = 'top bottom-=10%'
-	const fadeProperties = {
-		autoAlpha: 0,
-		delay: 0.2,
-		duration: 0.6,
-	}
-
-	const staggerProperties = {
-		autoAlpha: 0,
-		delay: 0.2,
-		stagger: 0.15,
-	}
-
-	/**
-	 * SINGLE FINS (Single Fade Ins)
-	 */
-
-	// FADE -------------------------------
+	// FADE ANIMATION ----------------------------
 	elementsFade.forEach((element) => {
+		const fadeType = element.getAttribute('data-fade')
+
+		const directionMap = {
+			up: { y: 16 },
+			down: { y: -16 },
+			left: { x: 16 },
+			right: { x: -16 },
+		}
+
+		const fadeDirection = directionMap[fadeType] || {}
+
 		const settings = {
 			scrollTrigger: {
 				trigger: element,
-				start: START,
+				start: 'top bottom-=10%',
 			},
-			...fadeProperties,
+			autoAlpha: 0,
+			delay: 0.2,
+			duration: 0.6,
+			...fadeDirection,
 		}
 
 		gsap.from(element, settings)
 	})
 
-	// FADE DOWN --------------------------
-	elementsFadeDown.forEach((element) => {
-		const settings = {
-			scrollTrigger: {
-				trigger: element,
-				start: START,
-			},
-			...fadeProperties,
-			y: -16,
+	// STAGGER ANIMATIONS -----------------------
+	elementsStagger.forEach((stagger) => {
+		const staggerType = stagger.getAttribute('data-stagger')
+
+		const directionMap = {
+			up: { y: 20 },
+			down: { y: -20 },
+			left: { x: 20 },
+			right: { x: -20 },
 		}
 
-		gsap.from(element, settings)
-	})
+		const staggerDirection = directionMap[staggerType] || {}
 
-	// FADE UP ----------------------------
-	elementsFadeUp.forEach((element) => {
-		const settings = {
-			scrollTrigger: {
-				trigger: element,
-				start: START,
-			},
-			...fadeProperties,
-			y: 16,
-		}
-
-		gsap.from(element, settings)
-	})
-
-	// FADE LEFT --------------------------
-	elementsFadeLeft.forEach((element) => {
-		const settings = {
-			scrollTrigger: {
-				trigger: element,
-				start: START,
-			},
-			...fadeProperties,
-			x: 40,
-		}
-
-		gsap.from(element, settings)
-	})
-
-	// FADE RIGHT -------------------------
-	elementsFadeRight.forEach((element) => {
-		const settings = {
-			scrollTrigger: {
-				trigger: element,
-				start: START,
-			},
-			...fadeProperties,
-			x: -40,
-		}
-
-		gsap.from(element, settings)
-	})
-
-	/**
-	 * STAG DOS (Do Staggers)
-	 */
-	elementsStaggerFade.forEach((stagger) => {
 		const elementsStagger = gsap.utils.toArray(stagger.children)
 
 		gsap.from(elementsStagger, {
 			scrollTrigger: {
 				trigger: stagger,
-				start: START,
+				start: 'top bottom-=10%',
 			},
-			...staggerProperties,
-		})
-	})
-
-	elementsStaggerLeft.forEach((stagger) => {
-		const elementsStagger = gsap.utils.toArray(stagger.children)
-
-		gsap.from(elementsStagger, {
-			scrollTrigger: {
-				trigger: stagger,
-				start: START,
-			},
-			...staggerProperties,
-			x: 20,
-		})
-	})
-
-	elementsStaggerRight.forEach((stagger) => {
-		const elementsStagger = gsap.utils.toArray(stagger.children)
-
-		gsap.from(elementsStagger, {
-			scrollTrigger: {
-				trigger: stagger,
-				start: START,
-			},
-			...staggerProperties,
-			x: -20,
-		})
-	})
-	elementsStaggerUp.forEach((stagger) => {
-		const elementsStagger = gsap.utils.toArray(stagger.children)
-
-		gsap.from(elementsStagger, {
-			scrollTrigger: {
-				trigger: stagger,
-				start: START,
-			},
-			...staggerProperties,
-			y: 20,
-		})
-	})
-	elementsStaggerDown.forEach((stagger) => {
-		const elementsStagger = gsap.utils.toArray(stagger.children)
-
-		gsap.from(elementsStagger, {
-			scrollTrigger: {
-				trigger: stagger,
-				start: START,
-			},
-			...staggerProperties,
-			y: -20,
+			autoAlpha: 0,
+			delay: 0.2,
+			stagger: 0.15,
+			...staggerDirection,
 		})
 	})
 }
