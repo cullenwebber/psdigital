61fcef796887dd4138beaaa560a9ebf2c719826a
Joe <jmgrant1995@gmail.com>
2024-12-03 15:53:06 +0800

    index.php and new pagination. rename partials

--- wp-content/themes/startdigital/index.php
+++ wp-content/themes/startdigital/index.php
@@ -1,24 +1,11 @@
 <?php
 
-
-/**
- * The main template file
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists
- *
- * Methods for TimberHelper can be found in the /lib sub-directory
- *
- * @package  WordPress
- * @subpackage  Timber
- * @since   Timber 0.1
- */
-
 use Timber\Timber;
 
 $context = Timber::context();
 $context['post'] = Timber::get_post();
 $context['posts'] = Timber::get_posts();
+$context['categories'] = array_filter(Timber::get_terms('category'), fn($category) => $category->name !== 'Uncategorized');
+$context['title'] = $context['post']->title;
 
-Timber::render('archive.twig', $context);
+Timber::render('index.twig', $context);
