30caa2811142655573c691e08e411f415498388e
Joe <jmgrant1995@gmail.com>
2025-01-21 15:14:12 +0800

    update header on scroll

--- wp-content/themes/startdigital/js/utils/headerOnScroll.js
+++ wp-content/themes/startdigital/js/utils/headerOnScroll.js
@@ -1,25 +1,23 @@
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
+
+/**
+ * Adds 'header-scrolling' class to header element when user scrolls past 100px from top
+ * Removes class when scrolling back up
+ *
+ * @returns {void}
+ * @requires ScrollTrigger
+ *
+ */
 export default function initHeaderOnScroll() {
 	const header = document.querySelector('header')
 	if (!header) return
-	let prevScrollpos = window.scrollY
 
-	window.onscroll = function () {
-		let currentScrollPos = window.scrollY
-
-		toggleScrollActive()
-		toggleScrollBackground()
-
-		// TOGGLE HEADER VISIBILITY
-		function toggleScrollActive() {
-			if (currentScrollPos < 100) {
-				header.classList.remove('header-scrolling')
-			} else if (prevScrollpos > currentScrollPos) {
-				header.classList.remove('header-scrolling')
-			} else {
-				header.classList.add('header-scrolling')
-			}
-		}
-
-		prevScrollpos = currentScrollPos
-	}
+	ScrollTrigger.create({
+		start: '100px top',
+		onEnter: () => header.classList.add('header-scrolling'),
+		onLeaveBack: () => header.classList.remove('header-scrolling'),
+	})
 }
