6e483d3ea0487f13684fce90d4ca11ad6f5d6427
cullenwebber <139840023+cullenwebber@users.noreply.github.com>
2025-08-13 21:10:25 +0800

    Footer updates

--- wp-content/themes/psdigital/js/components/footer-physics.js
+++ wp-content/themes/psdigital/js/components/footer-physics.js
@@ -1,13 +1,15 @@
 import SVGPhysics from '../utils/SVGPhysics.js'
 import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger.js'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 gsap.registerPlugin(ScrollTrigger)
 
 export default function initFooterPhysics() {
 	const width = window.innerWidth
 	let scale
 
-	if (width >= 640) {
+	if (width >= 1600) {
+		scale = 0.000575 * 1600
+	} else if (width >= 640) {
 		scale = 0.000575 * width
 	} else {
 		scale = 0.000875 * width
